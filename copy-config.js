0616713e9f1fa8d0b8aba2fde26d565def646754
Emil Okkels Klein <eok@pentia.dk>
2016-09-22 13:55:18 +0200

    removed copy install script

--- copy-config.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp")
-var path = require("path");
-var fs = require("fs");
-var cwd = require("cwd");
-
-var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
-var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
-
-gulp.src(node_solution_config).pipe(gulp.dest("../../", { overwrite: false }))
